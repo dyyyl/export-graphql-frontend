c0ef802bc384d17c2777f90d75991d110f1c59b4
Dyl <dyl@touche.local>
2021-04-16 11:07:54 +0200

    clean up on aisle graphql

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,7 +1,5 @@
-import { ApolloClient, HttpLink, InMemoryCache, split } from '@apollo/client';
-import { WebSocketLink } from '@apollo/client/link/ws';
+import { ApolloClient, InMemoryCache } from '@apollo/client';
 import { ApolloProvider } from '@apollo/client/react';
-import { getMainDefinition } from '@apollo/client/utilities';
 import { useState } from 'react';
 
 import awfulBaby from 'shared/assets/awful-baby.gif';
@@ -9,36 +7,9 @@ import awfulBaby from 'shared/assets/awful-baby.gif';
 import Main from 'shared/components/Main';
 import Title from 'shared/components/Title';
 
+import { splitLink } from 'shared/graphql/utils/splitLink';
+
 import GlobalStyle from 'shared/styles/GlobalStyle';
-
-const httpLink = new HttpLink({
-  uri: 'http://localhost:4000/graphql',
-});
-
-const wsLink = new WebSocketLink({
-  uri: 'ws://localhost:4000/subscriptions',
-  options: {
-    reconnect: true,
-  },
-});
-
-// The split function takes three parameters:
-//
-// * A function that's called for each operation to execute
-// * The Link to use for an operation if the function returns a "truthy" value
-// * The Link to use for an operation if the function returns a "falsy" value
-const splitLink = split(
-  ({ query }) => {
-    const definition = getMainDefinition(query);
-
-    return (
-      definition.kind === 'OperationDefinition' &&
-      definition.operation === 'subscription'
-    );
-  },
-  wsLink,
-  httpLink,
-);
 
 const client = new ApolloClient({
   link: splitLink,
---
+++ src/shared/graphql/utils/splitLink.ts
@@ -0,0 +1,32 @@
+import { HttpLink, split } from '@apollo/client';
+import { WebSocketLink } from '@apollo/client/link/ws';
+import { getMainDefinition } from '@apollo/client/utilities';
+
+const httpLink = new HttpLink({
+  uri: 'http://localhost:4000/graphql',
+});
+
+const wsLink = new WebSocketLink({
+  uri: 'ws://localhost:4000/subscriptions',
+  options: {
+    reconnect: true,
+  },
+});
+
+// The split function takes three parameters:
+//
+// * A function that's called for each operation to execute
+// * The Link to use for an operation if the function returns a "truthy" value
+// * The Link to use for an operation if the function returns a "falsy" value
+export const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+
+    return (
+      definition.kind === 'OperationDefinition' &&
+      definition.operation === 'subscription'
+    );
+  },
+  wsLink,
+  httpLink,
+);
